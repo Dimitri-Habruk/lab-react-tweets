1402889464313feedc409ca771c0116fee79043d
Dimitri <d.habruk@gmail.com>
2023-01-05 21:52:02 +0100

    lab done

---
+++ src/components/ProfileImage.js
@@ -0,0 +1,17 @@
+
+
+function ProfileImage(props) {
+return (
+    <>
+        <img
+            src={props.imgProfile}
+            className="profile"
+            alt="NON"
+        />
+    </>
+)
+
+}
+
+
+export default ProfileImage;
---
+++ src/components/User.js
@@ -0,0 +1,15 @@
+
+
+function User(props) {
+    return (
+        <>
+                       <span className="name">{props.usName}</span>
+
+        </>
+    )
+    
+    }
+    
+    
+    export default User;
+    
